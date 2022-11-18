fb93fab6860f5e27b34f7f7d8c5b8e8c9d707cd0
Dominguez <pilymdm@bu.edu>
2022-11-18 12:45:40 -0600

    feat: add basic app js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3002, ()=> {
+    console.log('Serving on port 3000')
+})
