e0d55654ebcb00204f6372f45d7faa91d81070fa
Dominguez <pilymdm@bu.edu>
2022-11-30 20:07:46 -0600

    docs: add header sections in main app

--- tinder-dog-app/src/App.js
+++ tinder-dog-app/src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1> Tinder Dog</h1>
+      {/* Header */}
+      {/* Tinder Cards */}
+      {/* Buttons below cards */}
+
+      {/* Chat screen */}
+      {/* Individual chat screen */}
     </div>
   );
 }
