dfe5f828294fd30526e5f3fbb7ccc14bfa955fc5
Dominguez <pilymdm@bu.edu>
2022-12-01 11:58:41 -0600

    feat: implement react router and switch

--- tinder-dog-app/src/App.js
+++ tinder-dog-app/src/App.js
@@ -1,16 +1,27 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
+import {BrowserRouter as Router, Switch,  Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header/>
+      <Router>
+        <Switch>
+          <Route path='/chats'>
+            <h1>Chats</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
       {/* Tinder Cards */}
       {/* Buttons below cards */}
-
       {/* Chat screen */}
       {/* Individual chat screen */}
+      </Router>
+      
     </div>
   );
 }
