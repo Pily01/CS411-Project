69e2b13f4f18a1bd79fd3596d018a45675ab01b0
Dominguez <pilymdm@bu.edu>
2022-12-01 11:41:33 -0600

    feat: add logo and iconButton elements

--- tinder-dog-app/src/Header.js
+++ tinder-dog-app/src/Header.js
@@ -2,13 +2,20 @@ import React from 'react'
 import './Header.css'
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import IconButton from '@mui/material/IconButton';
+import logo from'./TINDog.png'
 
 function Header() {
   return (
     <div className='header'>
-        <AccountBoxIcon/>
-        <h1> I am the main header</h1>
-        <QuestionAnswerIcon/>
+        <IconButton>
+            <AccountBoxIcon className='header__icon' fontSize='large'/>
+        </IconButton>
+        <img className='header__logo' src={logo} alt='logo'/>
+        <IconButton>
+            <QuestionAnswerIcon className='header__icon' fontSize='large'/>
+        </IconButton>
+        
     </div>
   )
 }
