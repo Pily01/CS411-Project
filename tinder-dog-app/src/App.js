89d95272d6f4e90bf217df25b4e0d51e4e6acee8
Dominguez <pilymdm@bu.edu>
2022-12-01 11:09:22 -0600

    feat: import material UI

--- tinder-dog-app/src/App.js
+++ tinder-dog-app/src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1> Tinder Dog</h1>
       {/* Header */}
+      <Header/>
       {/* Tinder Cards */}
       {/* Buttons below cards */}
 
